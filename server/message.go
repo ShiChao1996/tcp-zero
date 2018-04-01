40148d1631f95fe92cfcac099ee59347b47ebd70
ShiChao1996 <2483061998@qq.com>
2018-04-01 20:16:40 +0800

    change message's decode and encode methods

--- server/message.go
+++ server/message.go
@@ -32,42 +32,102 @@ package server
 import (
 	"encoding/binary"
 	"bytes"
+	"fmt"
+)
+
+const (
+	lenIdx = 4
+	msgIdx = 8
 )
 
 type message struct {
-	msg []byte
+	seq    uint32
+	length uint32
+	msg    []byte
 }
 
-func NewMsg(msg string) *message {
+// NewMsg generate message from msg string and seqNumber
+func NewMsg(seq uint32, msg string) *message {
 	return &message{
+		seq,
+		uint32(len([]byte(msg))),
 		[]byte(msg),
 	}
 }
 
-func (m *message) Encode() ([]byte, error) {
-	var err error
+func (m *message) Msg() []byte {
+	return m.msg
+}
 
-	m.msg, err = encode(m.msg)
+func (m *message) Len() int {
+	return len(m.msg)
+}
+
+// Encode encode message to []byte
+func (m *message) Encode() ([]byte, error) {
+	var (
+		err error
+		buf bytes.Buffer
+		b   = make([]byte, 4)
+	)
+
+	binary.BigEndian.PutUint32(b, m.seq)
+	buf.Write(b[:lenIdx])
+	binary.BigEndian.PutUint32(b, m.length)
+	buf.Write(b[:lenIdx])
+
+	b, err = encode(m.msg)
+	if err != nil {
+		return []byte{}, err
+	}
+	_, err = buf.Write(b)
 	if err != nil {
 		return []byte{}, err
 	}
 
-	return m.msg, nil
+	return buf.Bytes(), nil
 }
 
+
+// Decode decode []byte to message
 func (m *message) Decode(b []byte) error {
 	var err error
 
-	m.msg, err = decode(b)
+	seq, err := decode(b[:lenIdx], lenIdx)
 	if err != nil {
 		return err
 	}
 
+	m.seq = binary.BigEndian.Uint32(seq)
+
+	l, err := decode(b[lenIdx:msgIdx], lenIdx)
+	if err != nil {
+		return err
+	}
+	m.length = binary.BigEndian.Uint32(l)
+
+	m.msg, err = decode(b[msgIdx:], int32(m.length))
+
 	return nil
 }
 
-func (m *message) Msg() []byte {
-	return m.msg
+
+// DecodeMsgs decode multiple messages at once
+func DecodeMsgs(b []byte) (msgs []*message, err error) {
+	for {
+		msg := NewMsg(0, "")
+		err = msg.Decode(b)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		msgs = append(msgs, msg)
+		b = b[msgIdx+msg.Len():]
+		if len(b) == 0 {
+			return
+		}
+	}
 }
 
 func encode(b []byte) ([]byte, error) {
@@ -80,8 +140,8 @@ func encode(b []byte) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func decode(b []byte) ([]byte, error) {
-	var ret []byte
+func decode(b []byte, n int32) ([]byte, error) {
+	var ret = make([]byte, n)
 	buf := bytes.NewReader(b)
 
 	err := binary.Read(buf, binary.BigEndian, ret)
